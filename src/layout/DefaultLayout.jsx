d865d2e0bbc86201dfc1f521661e1b1d00140309
Diego Fagundes <fagundesdiego2015bolcombr@gmail.com>
2023-10-16 18:21:32 -0300

    correção do erro no default layout

--- src/layout/DefaultLayout.jsx
+++ src/layout/DefaultLayout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom'
 
-import Footer from '../components/Footer/footer'
+import Footer from '../components/Footer'
 import Header from '../components/Header/Header'
 
 function DefaultLayout() {
