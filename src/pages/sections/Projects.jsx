95d93c8363c33ec0c3e3b25823416086f050a4cb
Diego Fagundes <fagundesdiego2015bolcombr@gmail.com>
2025-06-03 12:21:39 -0300

    feat(projects): ordena projetos com base no índice para controlar a ordem no slider

--- src/pages/sections/Projects.jsx
+++ src/pages/sections/Projects.jsx
@@ -60,85 +60,89 @@ const Projects = () => {
                 : { '--swiper-navigation-size': '40px' }
             }
           >
-            {projects.sort((a, b) => a.index - b.index).map((project) => (
-              <SwiperSlide
-                className="relative mx-0.5 flex animate-toAppearPlus flex-col items-center justify-center bg-transparent text-center"
-                key={project.name}
-              >
-                <img
-                  onMouseOver={() => handleMouseOver(project.name)}
-                  onMouseOut={() => handleMouseOut(project.name)}
-                  style={{
-                    opacity: isMobileOrMouseOver
-                      ? '0.07'
-                      : isMouseOver[project.name]
-                        ? '0.07'
-                        : '1',
-                    transition: 'opacity 1s ease, z-index 0.3s ease'
-                  }}
-                  className="block h-full w-full rounded-md opacity-10 hover:opacity-80"
-                  src={project.imagem}
-                  alt={project.alt}
-                />
-                <div className="absolute top-16 flex w-full flex-col items-center gap-2 md:gap-3">
-                  <div
+            {projects
+              .sort((a, b) => a.index - b.index)
+              .map((project) => (
+                <SwiperSlide
+                  className="relative mx-0.5 flex animate-toAppearPlus flex-col items-center justify-center bg-transparent text-center"
+                  key={project.name}
+                >
+                  <img
                     onMouseOver={() => handleMouseOver(project.name)}
                     onMouseOut={() => handleMouseOut(project.name)}
                     style={{
                       opacity: isMobileOrMouseOver
-                        ? '1'
+                        ? '0.07'
                         : isMouseOver[project.name]
-                          ? '1'
-                          : '0',
-                      transition: 'opacity 1s ease'
+                          ? '0.07'
+                          : '1',
+                      transition: 'opacity 1s ease, z-index 0.3s ease'
                     }}
-                    className="div-info flex w-3/5 flex-col md:gap-3"
-                  >
-                    <h5 className="text-lg">{project.name}</h5>
-                    {project.status ? (
-                      <span className="text-rose-400">{project.status}</span>
-                    ) : (
-                      ''
-                    )}
-                    <p className="mb-[6px] text-xs leading-none md:text-sm">
-                      {project.description}
-                    </p>
-                    <p className="text-xs leading-none md:text-sm">
-                      <span className="text-rose-400">Tecnologias:</span>
-                      <br />
-                      {project.technologies || project.frontendTechnologies}
-                      <br />
-                      <br />
-                      {project.backendTechnologies}
-                    </p>
-                  </div>
-                  {project.project_link && (
-                    <div className="flex flex-row items-center justify-center gap-[.3rem] md:gap-1">
-                      <button className="flex h-auto w-10 items-center justify-center rounded-full bg-blue-500 p-1 hover:bg-purple-500 active:bg-purple-700 md:w-20">
-                        <a
-                          className="text-sss text-white md:text-xs"
-                          target="_blank"
-                          href={project.project_link}
-                          rel="noreferrer"
-                        >
-                          Projeto
-                        </a>
-                      </button>
-                      <button className="flex h-auto w-10 items-center justify-center rounded-full bg-blue-500 p-1 hover:bg-purple-500 active:bg-purple-700 md:w-20">
-                        <a
-                          className="text-sss text-white md:text-xs"
-                          target="_blank"
-                          href={project.repository_link}
-                          rel="noreferrer"
-                        >
-                          Repositório
-                        </a>
-                      </button>
+                    className="block h-full w-full rounded-md opacity-10 hover:opacity-80"
+                    src={project.imagem}
+                    alt={project.alt}
+                  />
+                  <div className="absolute top-16 flex w-full flex-col items-center gap-2 md:gap-3">
+                    <div
+                      onMouseOver={() => handleMouseOver(project.name)}
+                      onMouseOut={() => handleMouseOut(project.name)}
+                      style={{
+                        opacity: isMobileOrMouseOver
+                          ? '1'
+                          : isMouseOver[project.name]
+                            ? '1'
+                            : '0',
+                        transition: 'opacity 1s ease'
+                      }}
+                      className="div-info flex w-3/5 flex-col md:gap-3"
+                    >
+                      <h5 className="text-lg">{project.name}</h5>
+                      {project.status ? (
+                        <span className="text-rose-400">{project.status}</span>
+                      ) : (
+                        ''
+                      )}
+                      <p className="mb-[6px] text-xs leading-none md:text-sm">
+                        {project.description}
+                      </p>
+                      <p className="text-xs leading-none md:text-sm">
+                        <span className="text-rose-400">Tecnologias:</span>
+                        <br />
+                        {project.technologies || project.frontendTechnologies}
+                        <br />
+                        <br />
+                        {project.backendTechnologies}
+                      </p>
                     </div>
-                  )}
-                </div>
-              </SwiperSlide>
-            ))}
+                    {project.project_link && (
+                      <div className="flex flex-row items-center justify-center gap-[.3rem] md:gap-1">
+                        <button className="flex h-auto w-12 items-center justify-center rounded-full bg-blue-500 p-1 hover:bg-purple-500 active:bg-purple-700 md:w-28">
+                          <a
+                            className="text-sss text-white md:text-xs"
+                            target="_blank"
+                            href={project.project_link}
+                            rel="noreferrer"
+                          >
+                            Ver Projeto
+                          </a>
+                        </button>
+                        {project.repository_link && (
+                          <button className="flex h-auto w-12 items-center justify-center rounded-full bg-blue-500 p-1 hover:bg-purple-500 active:bg-purple-700 md:w-28">
+                            <a
+                              className="text-sss text-white md:text-xs"
+                              target="_blank"
+                              href={project.repository_link}
+                              rel="noreferrer"
+                            >
+                              Ver Repositório
+                            </a>
+                          </button>
+                        )}
+                      </div>
+                    )}
+                  </div>
+                </SwiperSlide>
+              ))}
           </Swiper>
         </div>
       </AnimatedSection>
